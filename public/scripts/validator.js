2d1189f786caaee5bf3d6dd3f55465ebfdf1bc12
Gustavo Sales <gustavo@gsales.io>
2023-05-22 21:57:31 -0300

    feat: login page

---
+++ public/scripts/validator.js
@@ -0,0 +1,29 @@
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+
+          return;
+        }
+
+        form.classList.add('was-validated');
+
+        alert('O formulário está correto e pode ser enviado');
+
+        return false;
+      },
+      false
+    );
+  });
+})();
